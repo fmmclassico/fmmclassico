2780af742c25b3584cba67e975805dcff0f7c6ae
Miss Phaedra <lovelyfedramartha@gmail.com>
2026-02-20 20:59:17 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from './utils';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { 
   Home, 
   ShoppingCart, 

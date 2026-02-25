9a841353ec67560c8d8f813c7827c90f49fcefb0
FMM CLASSICO <fmmclassico@gmail.com>
2026-02-25 09:02:53 +0000

    File changes

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { base44 } from '@/api/base44Client';
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { CheckCircle2, Package, MapPin, Loader2, ExternalLink, Bell, CheckIcon } from 'lucide-react';
+import { CheckCircle2, Package, Loader2, Bell, CheckIcon, Upload, ImageIcon } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { toast } from 'sonner';
 

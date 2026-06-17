a5fd94458364da20aee8b38fd54765b386807272
FMM CLASSICO <fmmclassico@gmail.com>
2026-06-17 13:45:03 +0000

    Manual code change

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -183,4 +183,12 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const us
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+};

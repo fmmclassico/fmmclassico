9923bc4d0e4eb7306c08d978effb115faf5cb0f4
fmmclassico <fmmclassico@gmail.com>
2026-06-25 20:03:27 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
+import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    base44({
+      legacySDKImports: false,
+      hmrNotifier: false,
+      navigationNotifier: false,
+      visualEditAgent: false
+    }),
+    react(),
+  ],
 })

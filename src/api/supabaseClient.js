aeb5a37fe943eda26d623842100c708692e127d3
fmmclassico <fmmclassico@gmail.com>
2026-06-27 12:07:33 +0000

    Update supabaseClient.js

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://kptlejtauwqvaapsrjfx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwdGxlanRhdXdxdmFhcHNyamZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDcxNzQsImV4cCI6MjA5Nzc4MzE3NH0.ghtyNablJiYmFUboLPzup48oT-byQV7tqHIjPndCW8k'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwdGxlanRhdXdxdmFhcHNyamZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDcxNzQsImV4cCI6MjA5Nzc4MzE3NH0.ghtyNablJiYmFUboLPzup48oT-byQV7tqHIjPndCW8k'
 )

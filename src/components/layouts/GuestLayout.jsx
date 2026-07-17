0e6513482d55fb6b3b4bef636cd5284227bfb105
fmmclassico <fmmclassico@gmail.com>
2026-07-17 13:33:29 +0000

    Describe the changes you made

--- src/components/layouts/GuestLayout.jsx
+++ src/components/layouts/GuestLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import HeaderSearchBar from '@/components/search/HeaderSearchBar.jsx';
+import HeaderSearchBar from '@/components/search/HeaderSearchBar';
 import { createPageUrl } from '@/lib/utils';
 import { 
   Search, 

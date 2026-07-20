4b30b97fa56e60b9070a2798d838c8b59e8195f8
fmmclassico <fmmclassico@gmail.com>
2026-07-20 11:50:34 +0000

    Remove base44 plugin from Vite configuration
    Removed base44 plugin configuration from Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    base44({
-      legacySDKImports: false,
-      hmrNotifier: false,
-      navigationNotifier: false,
-      visualEditAgent: false
-    }),
-    react(),
-  ],
+  plugins: [react()],
 })

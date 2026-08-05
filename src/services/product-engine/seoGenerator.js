91073439116b79a75299e758cd6c2b880a294588
fmmclassico <fmmclassico@gmail.com>
2026-08-05 14:17:34 +0000

    Add SEO generator function for product listings

---
+++ src/services/product-engine/seoGenerator.js
@@ -0,0 +1,16 @@
+export function generateSEO(product){
+
+
+return {
+
+title:
+`${product.name} Price in Ghana | FMM CLASSICO`,
+
+
+description:
+`Buy ${product.name} in Ghana from FMM CLASSICO. Genuine products, secure payment, fast delivery and competitive prices.`
+
+};
+
+
+}

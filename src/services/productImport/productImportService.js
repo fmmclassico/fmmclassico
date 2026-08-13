7f1d8d337c4f6bdecb4b73efd78e0a53224c49bb
fmmclassico <fmmclassico@gmail.com>
2026-08-13 15:45:47 +0000

    Add createImportId function for unique ID generation

--- src/services/productImport/productImportService.js
+++ src/services/productImport/productImportService.js
@@ -117,6 +117,8 @@ const PRESET_COLORS = ['Black', 'White', 'Blue', 'Red', 'Green', 'Silver', 'Gold
 const PRESET_WATTAGES = ['5W', '10W', '18W', '20W', '25W', '33W', '45W', '65W', '100W', '120W', '150W'];
 const PRESET_TYPES = ['USB-C', 'Lightning', 'Micro USB', 'Type-A', 'Wireless', 'Original', 'Compatible', 'Standard', 'Pro', 'Plus', 'Max'];
 
+
+
 function createImportId() {
   return typeof crypto !== 'undefined' && crypto.randomUUID
     ? crypto.randomUUID()
